a64220b7292e89f0002c1297974657df984d0ed6
Ehwa2006 <165359063+Ehwa2006@users.noreply.github.com>
2025-12-10 21:01:19 +0900

    Add files via upload
    Signed-off-by: Ehwa2006 <165359063+Ehwa2006@users.noreply.github.com>

--- JS_sorce.js
+++ JS_sorce.js
@@ -280,9 +280,11 @@ $('#slideNext').addEventListener('click', ()=> showSlide(slideIndex + 1));
 
 
 async function loadSerpBg(){
-  const vercelUrl = "https://ehwa2006.vercel.app/api/game-images";
+  const vercelUrl = "https://ehwa2006-github-io.vercel.app/api/game-images";
   const localProxyUrl = "http://localhost:3000/api/game-images";
   const timeoutMs = 2000;
+  // Debug: log which Vercel URL this script will call and script version
+  console.log('DEBUG scriptVersion:v3, vercelUrl=', vercelUrl);
   const timeout = (ms) => new Promise((_, rej) => setTimeout(() => rej(new Error('timeout')), ms));
 
   const urls = [vercelUrl, localProxyUrl];
